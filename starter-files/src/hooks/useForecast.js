1d4785d20ec26129b7f6dd277efb242f4baf5d3e
SanjanaaZola <zolasanjanaa@gail.com>
2021-10-11 00:21:56 +0530

    get weather changes

--- starter-files/src/hooks/useForecast.js
+++ starter-files/src/hooks/useForecast.js
@@ -10,10 +10,22 @@ const useForecast = () => {
     const [Loading, setLoading] = useState(false);
     const [Forecast, setForecast] = useState(null);
 
-    const submitRequest = async location => {
+    const getWoeid = async location => {
         const { data } = await axios(`${REQUEST_URL}/search`, { params: { query: location } });
 
         console.log({ data });
+
+        if (!data || data.length === 0) {
+            setError('There is no such location');
+            return;
+        }
+    };
+
+    const submitRequest = async location => {
+        const data = await getWoeid(location);
+
+        const response = await axios(`${REQUEST_URL}/${data[0].woeid}`);
+        console.log({ response });
     };
 
     return {
