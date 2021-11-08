92d8f319c16dae29934830fb67b4b13e8bad6b6b
SanjanaaZola <zolasanjanaa@gail.com>
2021-11-08 15:31:56 +0530

    current day update

--- starter-files/src/components/Page/Page.jsx
+++ starter-files/src/components/Page/Page.jsx
@@ -5,27 +5,33 @@ import Form from '../Form';
 import Error from '../Error';
 import Loader from '../Loader';
 import Forecast from '../Forecast';
+
 import useForecast from '../../hooks/useForecast';
 
 import styles from './Page.module.css';
+
 const Page = () => {
-    const { Error, Loading, Forecast, submitRequest } = useForecast();
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
+
     const onSubmit = value => {
         submitRequest(value);
     };
+
     return (
         <Fragment>
             <Header />
-            <div className={styles.box}>
-                {/* form */}
-                {!Loading && <Form submitSearch={onSubmit} />}
-                {/* error */}
-                {Error && <Error message={Error} />}
-                {/* loader */}
-                {Loading && <Loader />}
-            </div>
-            {/* forecast */}
-            {Forecast && <Forecast />}
+            {!forecast && (
+                <div className={`${styles.box} position-relative`}>
+                    {/* Form */}
+                    {!isLoading && <Form submitSearch={onSubmit} />}
+                    {/* Error */}
+                    {isError && <Error message={isError} />}
+                    {/* Loader */}
+                    {isLoading && <Loader />}
+                </div>
+            )}
+            {/* Forecast */}
+            {forecast && <Forecast forecast={forecast} />}
         </Fragment>
     );
 };
--- starter-files/src/hooks/useForecast.js
+++ starter-files/src/hooks/useForecast.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+import getCurrentDayForecast from '../helpers/getCurrentDayForecast';
+import getCurretnDayDetailedForecast from '../helpers/getCurrentDayDetailedForecast';
+import getUpcomingDaysForecast from '../helpers/getUpcomingDaysForecast';
+
 const BASE_URL = 'https://www.metaweather.com/api/location';
 const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
 const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
@@ -13,19 +17,47 @@ const useForecast = () => {
     const getWoeid = async location => {
         const { data } = await axios(`${REQUEST_URL}/search`, { params: { query: location } });
 
-        console.log({ data });
-
         if (!data || data.length === 0) {
             setError('There is no such location');
+            setLoading(false);
             return;
         }
+
+        return data[0];
+    };
+
+    const getForecastData = async woeid => {
+        const { data } = await axios(`${REQUEST_URL}/${woeid}`);
+
+        if (!data || data.length === 0) {
+            setError('Something went wrong');
+            setLoading(false);
+            return;
+        }
+        
+        return data;
+    };
+
+    const gatherForecastData = data => {
+        const currentDay = getCurrentDayForecast(data.consolidated_weather[0], data.title);
+        const currentDayDetails = getCurretnDayDetailedForecast(data.consolidated_weather[0]);
+        const upcomingDays = getUpcomingDaysForecast(data.consolidated_weather);
+
+        setForecast({ currentDay, currentDayDetails, upcomingDays });
+        setLoading(false);
     };
 
     const submitRequest = async location => {
-        const data = await getWoeid(location);
+        setLoading(true);
+        setError(false);
 
-        const response = await axios(`${REQUEST_URL}/${data[0].woeid}`);
-        console.log({ response });
+        const response = await getWoeid(location);
+        if (!response?.woeid) return;
+
+        const data = await getForecastData(response.woeid);
+        if (!data) return;
+
+        gatherForecastData(data);
     };
 
     return {
